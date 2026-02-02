ddfaf0fa6b958555a00632b34119f2e9ee029830
Crazy Bob <benclarknfk@gmail.com>
2026-02-02 22:39:30 +0000

    Change rent modal backdrop from black to lighter gray for better appearance

--- app/tools/[id]/page.tsx
+++ app/tools/[id]/page.tsx
@@ -433,7 +433,7 @@ export default function ToolDetailPage() {
             <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
               {/* Backdrop */}
               <div 
-                className="absolute inset-0 bg-black bg-opacity-50"
+                className="absolute inset-0 bg-gray-900 bg-opacity-40"
                 onClick={handleCloseModal}
               />
               
