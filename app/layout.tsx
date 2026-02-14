432d22997f277d028c10dc40c537503ea8be1948
Crazy Bob <benclarknfk@gmail.com>
2026-02-14 21:34:16 +0000

    update: change banner to development message with support email

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,10 +36,10 @@ export default function RootLayout({
           <CsrfInitializer />
           <div className="flex flex-col min-h-screen">
             <Header />
-            <div className="bg-red-50 border-b-2 border-red-300 px-4 py-3 text-center text-red-800">
+            <div className="bg-blue-50 border-b-2 border-blue-300 px-4 py-3 text-center text-blue-800">
               <p className="text-sm font-medium">
-                We're aware of a signup issue affecting some users. We're actively working on a fix. If you're unable to sign up, please contact{' '}
-                <a href="mailto:support@toolunity.co.uk" className="underline hover:text-red-900 font-semibold">
+                This website is in development. If you encounter any problems, please contact{' '}
+                <a href="mailto:support@toolunity.co.uk" className="underline hover:text-blue-900 font-semibold">
                   support@toolunity.co.uk
                 </a>
               </p>
